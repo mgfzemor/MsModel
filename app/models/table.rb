a484fa99b1e3c0fc252b1eb6e2105187a59f89a2
RafaAudibert <rafaeelaudibert@gmail.com>
2018-11-30 14:40:59 -0200

    :construction: Update some code to make more ruby-like

--- app/models/table.rb
+++ app/models/table.rb
@@ -13,42 +13,41 @@ class Table < ApplicationRecord
   end
 
   def newline
-    string = '<br/>'
+    '<br/>'
   end
 
   def indent
-    string = '&ensp;&ensp;'
+    '&ensp;&ensp;'
   end
 
   def ms_database_name
-    prefix = self.type.prefix
-    prefix + self.database_name
+    self.type.prefix + self.database_name
   end
 
   def generate_table_name
-    table_name =  "self.table_name = ':" + self.ms_database_name + "'" + newline
+    "self.table_name = ':" + self.ms_database_name + "'" + newline
   end
 
   def generate_getter(attribute)
     header = 'def ' +  attribute.system_name + '(value)' + newline
     body =  indent + 'write_attribute(:' + attribute.ms_database_name + ', value)' + newline
     footer = 'end' + newline
-    getter = header + body + footer + newline
+    header + body + footer + newline
   end
 
   def generate_setter(attribute)
     header = 'def ' +  attribute.system_name + newline
     body =  indent + 'read_attribute(:' + attribute.ms_database_name + ')' + newline
     footer = 'end' + newline
-    setter = header + body + footer + newline
+    header + body + footer + newline
   end
 
   def set_null
-    string = ', null: false '
+    ', null: false '
   end
 
   def set_unique
-    string = ', unique: true'
+    ', unique: true'
   end
 
   def generate_scaffold
@@ -64,31 +63,23 @@ class Table < ApplicationRecord
   def generate_getters_and_setters
     code = generate_table_name + newline
     self.columns.each  do |column|
-      getter = generate_getter(column)
-      setter = generate_setter(column)
-      code.concat(getter)
-      code.concat(setter)
+      code.concat(generate_getter(column))
+      code.concat(generate_setter(column))
     end
+
     code
   end
 
   def set_constraints(column)
     constraints = ''
-    if column.nn #not null
-      constraints.concat(set_null)
-    end
-    if column.uq #unique
-      constraints.concat(set_unique)
-    end
+    constraints.concat(set_null) if column.nn #not null
+    constraints.concat(set_unique) if column.uq #unique
+
     constraints
   end
 
   def set_id(column)
-    id = ''
-    unless column.active_id
-      id = ', id: false'
-    end
-    id
+    column.active_id ? '' : ', id: false'
   end
 
   def create_migration_up
@@ -98,16 +89,16 @@ class Table < ApplicationRecord
     db_columns = ''
     self.columns.each do |c|
       db_columns.concat(indent+indent+ 't.type :' + c.ms_database_name)
-      constraints =  set_constraints(c)
+      constraints = set_constraints(c)
       db_columns.concat(constraints + newline)
     end
     db_columns.concat(indent + 'end' + newline)
-    migration_up = function + create_table + db_columns + 'end' + newline
+
+    function + create_table + db_columns + 'end' + newline
   end
 
   def create_migration_down
-    function = 'def down' + newline
-
+    'def down' + newline
   end
 
   def generate_migration
